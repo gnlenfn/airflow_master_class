1a9ca5e9e620508f376efcc8d573ba09adc4fb49
hong <akenfn513@gmail.com>
2023-11-16 14:32:22 +0900

    op_kwargs

--- dags/dags_python_with_args.py
+++ dags/dags_python_with_args.py
@@ -2,7 +2,7 @@ from airflow import DAG
 import pendulum
 
 from airflow.operators.python import PythonOperator
-from common.common_func import regist
+from common.common_func import register, register2
 
 with DAG(
     dag_id="dags_python_with_args",
@@ -11,10 +11,20 @@ with DAG(
     catchup=False
 ) as dag:
 
-    regist_t1 = PythonOperator(
+    register_t1 = PythonOperator(
         task_id="dags_python_with_args",
-        python_callable=regist,
+        python_callable=register,
         op_args=['kim', 'man', 'kr', 'seoul']
     )
 
-    regist_t1
+    register_t2 = PythonOperator(
+        task_id="dags_python_with_kwargs",
+        python_callable=register2,
+        op_args=['kim', 'man', 'kr', 'seoul'],
+        op_kwargs={
+            "email": "test@test.com",
+            "phone": "010-2020-1123"
+        }
+    )
+
+    register_t1 >> register_t2
