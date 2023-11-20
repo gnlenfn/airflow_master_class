c866e91b0f5df06ce27b3d83da41c1fc896caf21
hong <akenfn513@gmail.com>
2023-11-20 10:49:26 +0900

    airflow date

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import pendulum
+
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2023, 11, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+
+    @task(task_id="python_task")
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+
+    show_templates()
