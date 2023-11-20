3294076ba72911c2fd3b8a603a2adb0ed5a46f6e
hong <akenfn513@gmail.com>
2023-11-20 11:13:18 +0900

    python operator template

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 from airflow.decorators import task
 
 with DAG(
-    dag_id="dags_python_task_decorator",
+    dag_id="dags_python_template",
     schedule="0 2 * * 1",
     start_date=pendulum.datetime(2023, 11, 1, tz="Asia/Seoul"),
     catchup=False
