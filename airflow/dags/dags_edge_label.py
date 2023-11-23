025ff85647034622bb5decf6544814eda62a19a2
hong <akenfn513@gmail.com>
2023-11-23 15:57:11 +0900

    EdgeLabel

---
+++ airflow/dags/dags_edge_label.py
@@ -0,0 +1,35 @@
+import pendulum
+
+from airflow import DAG
+from airflow.utils.edgemodifier import Label
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="dags_edge_label",
+    schedule=None,
+    start_date=pendulum.datetime(2023, 11, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    empty_1 = EmptyOperator(
+        task_id="empty_1"
+    )
+    empty_2 = EmptyOperator(
+        task_id="empty_2"
+    )
+
+    empty_1 >> Label("1과 2 사이") >> empty_2
+
+    empty_3 = EmptyOperator(
+        task_id="empty_3"
+    )
+    empty_4 = EmptyOperator(
+        task_id="empty_4"
+    )
+    empty_5 = EmptyOperator(
+        task_id="empty_5"
+    )
+    empty_6 = EmptyOperator(
+        task_id="empty_6"
+    )
+
+    empty_2 >> Label("Start Branch") >> [empty_3, empty_4, empty_5] >> Label("End Branch") >> empty_6
