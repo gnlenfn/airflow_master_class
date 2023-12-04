0bdb9703e08df393d75699f25c8a1bae04d2d41b
hong <akenfn513@gmail.com>
2023-12-04 10:13:10 +0900

    custom hook

--- airflow/dags/dags_python_with_custom_hook_bulk_load.py
+++ airflow/dags/dags_python_with_custom_hook_bulk_load.py
@@ -14,13 +14,13 @@ with DAG(
         custom_postgres_hook.bulk_load(table_name=table_name, file_name=file_name, delimiter=',',
                                        is_header=True, is_replace=True)
 
-        insert_custom_hook = PythonOperator(
-            task_id="insert_custom_hook",
-            python_callable=insert_postgres,
-            op_kwargs={
-                'postgers_conn_id': 'conn-db-postgres-custom',
-                'table_name': 'TbCorona19CountStatus_bulk2',
-                'file_name': '/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") '
-                             '| ds_nodash}}/TbCorona19CountStatus.csv'
-            }
-        )
+    insert_custom_hook = PythonOperator(
+        task_id="insert_custom_hook",
+        python_callable=insert_postgres,
+        op_kwargs={
+            'postgres_conn_id': 'conn-db-postgres-custom',
+            'table_name': 'TbCorona19CountStatus_bulk2',
+            'file_name': '/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") '
+                         '| ds_nodash}}/TbCorona19CountStatus.csv'
+        }
+    )
